6c65c962dad4ceafc24e2f831c7213f741faaf98
Ian Earle <ian.s.m.earle@gmail.com>
2021-02-02 23:36:58 -0400

    reformatting main

--- main.py
+++ main.py
@@ -4,8 +4,8 @@ import xml.etree.ElementTree as ET
 import xmltodict
 
 PROJECT_PATH = Path(sys.path[0])
-RESOURCES_PATH = PROJECT_PATH.joinpath('resources/Tiles2x')
-RESOURCES_XML_PATH = RESOURCES_PATH.joinpath('Tiles2x.xml')
+RESOURCES_PATH = PROJECT_PATH.joinpath("resources/Tiles2x")
+RESOURCES_XML_PATH = RESOURCES_PATH.joinpath("Tiles2x.xml")
 
 # {'property': 'booleanX'}
 # {'property': 'fileNameSize'}
@@ -14,10 +14,13 @@ RESOURCES_XML_PATH = RESOURCES_PATH.joinpath('Tiles2x.xml')
 # {'property': 'frameEntries'}
 # {'property': 'numFrameEntries'}
 
+
 def main():
     print(RESOURCES_XML_PATH)
-    with open(RESOURCES_XML_PATH, 'r') as reader:
-        root = xmltodict.parse(reader.read())['java']['object']['void'][0]['array']['void']
+    with open(RESOURCES_XML_PATH, "r") as reader:
+        root = xmltodict.parse(reader.read())["java"]["object"]["void"][0]["array"][
+            "void"
+        ]
         for child in root:
             process(child)
             break
@@ -36,10 +39,12 @@ def main():
     # # for child in root[0][0][0][0].items:
     #     print(child.get('filename'))
 
-def process(root):
-    print(root['@index'])
-    for child in root['object']['void']:
-        print(child['@property'])
 
-if __name__ == '__main__':
+def process(root):
+    print(root["@index"])
+    for child in root["object"]["void"]:
+        print(child["@property"])
+
+
+if __name__ == "__main__":
     main()
